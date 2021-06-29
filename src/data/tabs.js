f1f446b662e6d8f58c657f80c11d3743b336b67e
Michaela Stefkova <37817290+michaelastefkova@users.noreply.github.com>
2021-06-29 12:18:24 +0100

    Create tabs.js

---
+++ src/data/tabs.js
@@ -0,0 +1,20 @@
+export const tabs = [
+	{
+		id: 1,
+		data: "overview",
+		text: "Overview",
+		mobile: "Overview",
+	},
+	{
+		id: 2,
+		data: "structure",
+		text: "Internal Structure",
+		mobile: "Structure",
+	},
+	{
+		id: 3,
+		data: "geology",
+		text: "Surface Geology",
+		mobile: "Geology",
+	},
+];
